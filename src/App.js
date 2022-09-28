93529622f5477ff9871d0f4f5c7b9be48b40b37b
m0tei <david.matei.md@gmail.com>
2022-09-28 20:13:44 +0300

    busuri si statii

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export class MapContainer extends Component {
         showingInfoWindow: false,
         activeMarker: {},
         selectedPlace: {},
+<<<<<<< Updated upstream
         busMarker: {}
     };
 
@@ -17,14 +18,43 @@ export class MapContainer extends Component {
         onValue (ref(db, "buses/" + 1), (snapshot) => {
             if (JSON.stringify(this.state.busesLocation) !== JSON.stringify([snapshot.val()])) {
                 this.state.busesLocation = [snapshot.val()];
+=======
+        busLocation: [],
+        busStation: [],
+    };
+
+    dataBase() {
+        onValue (ref(db, "buses"), (snapshot) => {
+            console.log('firebaseBUSES', snapshot.val());
+            if (JSON.stringify(this.busLocation) !== JSON.stringify(snapshot.val())) {
+>>>>>>> Stashed changes
                 this.setState({
                     showingInfoWindow: false,
                     activeMarker: {},
                     selectedPlace: {},
+<<<<<<< Updated upstream
                     busesLocation: [snapshot.val()],
                     busesLocationInitialised: true,
+=======
+                    busLocation: snapshot.val(),
+                    busStation: [],
+>>>>>>> Stashed changes
                 })
             }
+        });
+
+        onValue (ref(db, "stations"), (snapshot) => {
+            console.log('firebaseSTATIONS', snapshot.val());
+            //if (JSON.stringify(this.busStation) !== JSON.stringify(snapshot.val())) {
+                this.setState({
+                    showingInfoWindow: false,
+                    activeMarker: {},
+                    selectedPlace: {},
+                    busLocation: [],
+                    busStation: snapshot.val(),
+                })
+            //}
+            console.log("STAIONS STATE", this.busStation)
         });
     }
 
@@ -46,14 +76,36 @@ export class MapContainer extends Component {
 
     displayMarkers = () => {
         const busIcon = { url: 'https://img.icons8.com/material-outlined/344/bus.png', scaledSize: { width: 30, height: 30} };
+<<<<<<< Updated upstream
         if (this.state.busesLocation) {
             console.log('displayMarkers', this.state.busesLocation);
             return this.state.busesLocation.map((busLocation, index) => {
                 return <Marker onClick={this.onMarkerClick} title={busLocation.line} name={<div><h1 color="grey">{busLocation.line}</h1>{busLocation.id}</div>} icon={busIcon} position={{
+=======
+        if (this.state.busLocation) {
+            console.log('displayBusMarkers', this.state.busLocation);
+            return this.state.busLocation.map((busLocation, index) => {
+                return <Marker onClick={this.onMarkerClick} title={busLocation.line} name={<div><h1 color="grey">{busLocation.line}</h1>id:{busLocation.id}</div>} icon={busIcon} position={{
+>>>>>>> Stashed changes
                     lat: busLocation.lat,
                     lng: busLocation.lng
                 }}
                 />
+<<<<<<< Updated upstream
+=======
+            })
+        }
+    }
+
+    displayStationsMarkers = () => {
+        const stationIcon = { url: "https://i.im.ge/2022/09/27/1yWfsx.bus-station-marker.png", scaledSize: { width: 10, height: 10} };
+        return this.state.busStations.map((busStation, index) => {
+            return <Marker onClick={this.onMarkerClick} title={busStation.name} name={<div><h1 color="grey">{busStation.name}</h1></div>} icon={stationIcon} position={{
+                lat: busStation.lat,
+                lng: busStation.lng
+            }}
+            />
+>>>>>>> Stashed changes
         })
         }
     }
